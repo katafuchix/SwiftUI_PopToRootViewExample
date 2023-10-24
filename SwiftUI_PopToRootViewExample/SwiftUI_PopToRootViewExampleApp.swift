fe44cfb33b119b9882fb2327659d2ad58ebfd190
katafuchix <katafuchix@gmail.com>
2023-10-24 11:19:14 +0900

    Initial Commit

---
+++ SwiftUI_PopToRootViewExample/SwiftUI_PopToRootViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PopToRootViewExampleApp.swift
+//  SwiftUI_PopToRootViewExample
+//
+//  Created by cano on 2023/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PopToRootViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
